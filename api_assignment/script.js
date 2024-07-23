66822559d340c0a2b7a6f7c1cdac2e1b87fe67ce
muhammad-mohsin-siddique <muhammadmohsinsiddique789@gmail.com>
2024-07-23 14:53:19 +0500

    update the assignment file

--- api_assignment/script.js
+++ api_assignment/script.js
@@ -1,23 +1,35 @@
 
-const requestUrl = 'https://api.github.com/users/hiteshchoudhary'; // request URL
-document.querySelector('#getData').addEventListener('click', (e) => {
+ // request URL
+const form = document.querySelector('#getUsername');
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const username = document.getElementById('username').value;
+    const requestUrl = `https://api.github.com/users/${username}`;
+    console.log(requestUrl);
     const xhr = new XMLHttpRequest(); // Creat an Object of XMLHttpRequest
     xhr.open('GET', requestUrl); // Request not send yet we have to use the send() method
     xhr.onreadystatechange = function () {
         console.log(xhr.readyState);
         if (xhr.readyState === 4) {
             const data = JSON.parse(this.responseText); // convert string to JSON
-            const htmlContent = `
-            <div class="profile-card">
-                <img src="${data.avatar_url}" alt="Profile Picture">
-                <div class="info">
-                    <div class="name">${data.name}</div>
-                    <div class="followers">Followers: ${data.followers}</div>
-                </div>
-           </div>`;
-            document.body.innerHTML = htmlContent;
+            if(data.status !== '404'){
+                const htmlContent = `
+                <div class="profile-card">
+                    <img src="${data.avatar_url}" alt="Profile Picture">
+                    <div class="info">
+                        <div class="name">${data.name}</div>
+                        <div class="followers">Followers: ${data.followers}</div>
+                    </div>
+               </div>`;
+                document.querySelector('#card-container').innerHTML = htmlContent;
+            }else{
+                alert('User Not Found');
+                document.querySelector('#card-container').innerHTML = '';
+            }
         }
     }
     xhr.send();
-})
+
+});
 
